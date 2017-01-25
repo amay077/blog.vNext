160fba8cd7de85e2cf617dbd2655458a561fed07
amay077 <okuokuoku@gmail.com>
2017-01-25 21:48:08 +0900

    Add AppImgnizer what is img to amp-img convert tool by mono

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@ jekyll build
 cd _site
 git add .
 git commit -m "site update"
+find $(pwd) -type f -name "*.html" -print | xargs -0 mono ../AmpImgnizer/AmpImgnizer.exe
+git add .
+git commit -m "replace img to amp-img"
 echo "press enter to deploy"
 read
 git push
