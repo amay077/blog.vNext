72915bb83a1bf364b873cd2ae6b6757705eaeeb7
amay077 <okuokuoku@gmail.com>
2018-06-17 02:14:54 +0900

    Update build and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
+cd _site
+git reset --hard
+git pull
+
+cd ..
+git reset --hard
+git pull
+
 jekyll build
 cd _site
 git add .
@@ -5,7 +13,4 @@ git commit -m "site update"
 find $(pwd) -type f -name "*.html" -print | xargs -0 mono ../AmpImgnizer/AmpImgnizer.exe
 git add .
 git commit -m "replace img to amp-img"
-echo "press enter to deploy"
-read
 git push
-cd ..
